ec427f5397d9e038da3a6c814ba2b1890462f057
Yeamin Rabbi <44961810+YeaminRabbi@users.noreply.github.com>
2024-09-29 21:38:26 +0600

    Update EditPermission.php

--- app/Filament/Resources/PermissionResource/Pages/EditPermission.php
+++ app/Filament/Resources/PermissionResource/Pages/EditPermission.php
@@ -16,4 +16,9 @@ class EditPermission extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+
+     // protected function afterSave(): void
+    // {
+    //     $this->redirect(FaqCategoryResource::getUrl('index'));
+    // }
 }
